f33462cd8416eccb4d14d26681cabe7c6565e0ed
Sujan Dhakal <sujan.dhakal.offical@gmail.com>
2024-08-25 12:22:34 +0545

    created layout for project and added shedcn

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+import {cn} from '@/lib/utils'
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600"],
+  variable: '--font-sans'
+});
+
+export const metadata: Metadata = {
+  title: "Svastha",
+  description: "A Health Care Management System",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased',  fontSans.variable)}>{children}</body>
+    </html>
+  );
+}
